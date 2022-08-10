a6de286c1eaa12f5b425f0dbd47fb24268f53e5f
Michael Pearce <mgpearce48@gmail.com>
2022-08-10 22:09:23 +1000

    Update build.sh

--- react/blogapi/build.sh
+++ react/blogapi/build.sh
@@ -1,1 +1,14 @@
 
+set -x
+npm install react/blogapi/
+npm run build
+npm start &
+sleep 1
+echo $! > .pidfile
+set +x
+
+input message: 'Finished reviewing the build stage? (Click "Proceed" to continue)'
+
+set -x
+kill $(cat .pidfile)
+set +x
