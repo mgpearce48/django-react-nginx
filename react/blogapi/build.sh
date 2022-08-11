c3549f7671150a578b0ab3a3f9f48c20dba8e2bd
Michael Pearce <mgpearce48@gmail.com>
2022-08-11 18:43:12 +1000

    Delete build.sh

--- react/blogapi/build.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env sh
-
-set -x
-npm --prefix /var/jenkins_home/workspace/django-react-nginx/react/blogapi run build
-npm --prefix /var/jenkins_home/workspace/django-react-nginx/react/blogapi start &
-sleep 1
-echo $! > .pidfile
-set +x
